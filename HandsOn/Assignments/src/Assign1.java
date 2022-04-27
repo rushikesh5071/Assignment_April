01ad49e304227bb3706c2f1274a18f73f17b90f0
rushikesh5071 <81679042+rushikesh5071@users.noreply.github.com>
2022-04-27 17:49:23 +0530

    Add files via upload

--- HandsOn/Assignments/src/Assign1.java
+++ HandsOn/Assignments/src/Assign1.java
@@ -3,40 +3,43 @@ import java.util.*;
 public class Assign1 {
 	
  
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TreeMap<String, Integer> map = new TreeMap<String, Integer>();
-		map.put("A", 80);
-		map.put("B", 75);
-		map.put("C", 72);
-		map.put("D", 70);
-		map.put("E", 82);
-		map.put("F", 78);
-		map.put("A", 85);
-		//map.put("G", 90);
+		
+		Scanner sc = new Scanner(System.in);
+		TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
 		
 		   
-		System.out.println("TreeMap elements:");
-		System.out.println(map);
-		
-		
-		Map<String, Integer> reverseTreeMap = map.descendingMap();
-		
-		System.out.println("ReverseTreeMap elements:");
-		System.out.println(reverseTreeMap);
-	}
-	public static boolean checkMap(TreeMap<String,Integer>map, String name, int value) {
-			if(!map.containsKey(name) && !map.containsValue(value)) {
-				System.out.println(name);
-				System.out.println(value);
-			
-			return true;
-			}
-		else {
-			System.out.println("Key already present:");
+		System.out.println("Enter the number of values to be stored  in Map");
 	
-		return false;
+		
+		int num = sc.nextInt();
+		
+		for(int i=0; i<num; i++) {
+			int valueAdd = i+1;
+			
+			System.out.println("Enter value for entry number"+valueAdd+":");
+			
+			int value = sc.nextInt();
+			
+			if(map.containsValue(value)) {
+				
+				System.out.println("Value already present cannot be added to Map, enter unique value");
+				
+				i--;
+			}
+			else 
+				map.put(i, value);
 		}
+		
+		System.out.println("Map elements:");
+		
+		System.out.println(map.values());
+		
+		System.out.println("Map elements in descending order:");
+		
+		System.out.println(map.descendingMap().values());
    }
  }
 
